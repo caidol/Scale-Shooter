28403b00a0c75f1c2c4655ee624e7b3ffcaad782
Myriad74 <acatchpool@protonmail.com>
2023-11-08 21:46:53 +0000

    Fixed player jump logic

--- Scale_Shooter/Scripts/player.gd
+++ Scale_Shooter/Scripts/player.gd
@@ -12,50 +12,48 @@ func _process(_delta):
 	process_input()
 
 func process_input():
+	# Allow the player to change jump direction in the air
+	if !is_on_floor():
+		if (Input.is_action_pressed("D") and prev_dir != 1):
+			sprite_animation.set_animation("JumpRight")
+			prev_dir = 1
+		elif (Input.is_action_pressed("A") and prev_dir != 0):
+			sprite_animation.set_animation("JumpLeft")
+			prev_dir = 0
+	
+	# Map the standard movement keys including jumping
 	if (Input.is_action_pressed("Space")):
 		if prev_dir == 1:
 			sprite_animation.set_animation("JumpRight")
 		else:
 			sprite_animation.set_animation("JumpLeft")
-	elif (Input.is_action_pressed("A")):
+	elif (Input.is_action_pressed("A") and is_on_floor()):
 		sprite_animation.set_animation("MoveLeft")
 		prev_dir = 0
-	elif (Input.is_action_pressed("D")):
+	elif (Input.is_action_pressed("D") and is_on_floor()):
 		sprite_animation.set_animation("MoveRight")
 		prev_dir = 1
 	else:
-		if prev_dir == 1:
-			sprite_animation.set_animation("IdleRight")
-		else:
-			sprite_animation.set_animation("IdleLeft")
+		if is_on_floor():
+			if prev_dir == 1:
+				sprite_animation.set_animation("IdleRight")
+			else:
+				sprite_animation.set_animation("IdleLeft")
 	
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
-		
-		'''
 		if prev_dir == 1:
-			sprite_animation.set_animation("JumpRight")
+			sprite_animation.animation = "JumpRight"
 		else:
-			sprite_animation.set_animation("JumpLeft")
-		'''
+			sprite_animation.animation = "JumpLeft"
+			
+		velocity.y += gravity * delta
 
 	# Handle Jump.
 	if Input.is_action_pressed("Space") and is_on_floor():
-		'''
-		print("test0")
-		if prev_dir == 1:
-			print("test1")
-			sprite_animation.set_animation("JumpRight")
-		else:
-			print("test2")
-			sprite_animation.set_animation("JumpLeft")
-		'''
-
 		velocity.y = JUMP_VELOCITY
-		
-		
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("A", "D")
