8b00350721d1f9e7f62b1816dd3cd95a93cdc38c
Myriad74 <acatchpool@protonmail.com>
2023-11-15 22:46:10 +0000

    Add scale limit to the weapon

--- Scale_Shooter/Scripts/Items/weapon.gd
+++ Scale_Shooter/Scripts/Items/weapon.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
+# constant defs
 const angular_velocity: float = 0.3
-const gun_fixed_point: Vector2 = Vector2(125, -13) 
+const weapon_radius: int = 120
+const scale_lower_bound: Vector2 = Vector2(0.5, 0.5)
+const scale_upper_bound: Vector2 = Vector2(4.5, 4.5)
+const gun_fixed_point: Vector2 = Vector2(120, -15) 
+
 @onready var player_node: CharacterBody2D = $".." # Retrieve player node to interact with it
 @onready var weapon_node: Node2D = $"."
 
@@ -15,22 +20,27 @@ func _process(_delta):
 	scale_weapon()
 	
 	# Rotate the weapon around a circular motion at a fixed radius length from the player
-	
-	var mouse_pos = get_global_mouse_position()
-	
+	rotate_weapon()
 
 func scale_weapon():
 	# Calculate the offset from the fixed point
 	var offset = position - gun_fixed_point
 	
-	if (Input.is_action_just_released("Mouse wheel up")):
+	if (Input.is_action_just_released("Mouse wheel up") && weapon_node.scale < scale_upper_bound):
 		weapon_node.scale += Vector2(0.5, 0.5)
 		
 		# Subtract offset position to maintain fixed point
 		position = gun_fixed_point - offset * Vector2(0.1, 0.1)
 		
-	elif (Input.is_action_just_released("Mouse wheel down")):
+	elif (Input.is_action_just_released("Mouse wheel down") && weapon_node.scale > scale_lower_bound):
 		weapon_node.scale -= Vector2(0.5, 0.5)
 		
 		# Add offset to adjust position to maintain a fixed point
 		position = gun_fixed_point + offset * Vector2(0.1, 0.1)
+
+func rotate_weapon():
+	# Retrieve the current mouse position and angle to mouse
+	var mouse_position = get_global_mouse_position()
+	var mouse_angle = player_node.position.angle_to(mouse_position)
+	
+	# set the weapon position relative to mouse position
